e6f69991e66ea5cbc3cef816c91c9383b08584d7
mayurpatil0906 <mayurpatill0906@gmail.com>
2026-03-22 14:37:51 +0530

    Apply Conversion Factor or Formula

--- js/conversion.js
+++ js/conversion.js
@@ -1,22 +1,42 @@
 import { getConversion } from "./api.js";
 
+
+function applyConversion(value, convObj) {
+
+    // invalid number
+    if (!Number.isFinite(value)) {
+        throw new Error("Invalid number");
+    }
+
+    // SAME UNIT
+    if (convObj.from === convObj.to) {
+        return value;
+    }
+
+    // FACTOR CASE
+    if (convObj.factor !== null) {
+        return parseFloat((value * convObj.factor).toFixed(6));
+    }
+
+    // FORMULA CASE
+    if (convObj.formula) {
+        try {
+            const expr = convObj.formula.replace(/x/g, value);
+            return parseFloat(eval(expr).toFixed(6));
+        } catch (error) {
+            throw new Error("Bad formula");
+        }
+    }
+
+    throw new Error("Invalid conversion object");
+}
 export async function convertValue(value, from, to) {
     try {
 
         const conversion = await getConversion(from, to);
 
-        // FACTOR CASE
-        if (conversion.factor !== null) {
-            return value * conversion.factor;
-        }
-
-        // FORMULA CASE (temperature)
-        if (conversion.formula) {
-            const formula = conversion.formula.replace(/x/g, value);
-            return eval(formula);
-        }
-
-        throw new Error("Invalid conversion");
+        
+        return applyConversion(value, conversion);
 
     } catch (error) {
         alert("Conversion not available for this pair");
